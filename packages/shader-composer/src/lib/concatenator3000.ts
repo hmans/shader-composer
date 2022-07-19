2af0b452c5d7ab109eb1483ab962ceccc1e92dbf
Hendrik Mans <hendrik@mans.de>
2022-07-19 09:12:30 +0200

    More 3000

--- packages/shader-composer/src/lib/concatenator3000.ts
+++ packages/shader-composer/src/lib/concatenator3000.ts
@@ -1,7 +1,12 @@
 export type Part = any
 
-export const concatenate = (...parts: Part[]) => compact(parts).join("\n")
+export const concatenate = (...parts: Part[]): string =>
+	compact(parts)
+		.flat(Infinity)
+		.join("\n")
 
-export const compact = (parts: Part[]) => parts.filter((p) => !!p)
+export const identifier = (...parts: Part[]): string => compact(parts).join("_")
 
-export const identifier = (...parts: Part[]) => compact(parts).join("_")
+export const compact = (parts: Part[]): Part[] => parts.filter((p) => !!p)
+
+export const block = (...parts: Part[]): Part[] => ["{", ...parts, "}"]
