0ad6908b58050201b96cf44b146cba1ec973cbd7
Hendrik Mans <hendrik@mans.de>
2022-07-19 17:32:50 +0200

    Play around with textures some more

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -14,7 +14,7 @@ import {
 	Time,
 	UV
 } from "shader-composer"
-import { Color, MeshStandardMaterial, RepeatWrapping, TextureDataType } from "three"
+import { Color, MeshStandardMaterial, RepeatWrapping, Vector2 } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 
 export default function Playground() {
@@ -23,13 +23,16 @@ export default function Playground() {
 	texture.wrapT = RepeatWrapping
 
 	const [{ uniforms, ...shader }, update] = useMemo(() => {
-		const tex2d = Texture2D(Sampler2D("u_texture"), TilingUV(Add(UV, Div(Time, 10))))
+		const tex2d = Texture2D(
+			Sampler2D("u_texture"),
+			TilingUV($`vec2(${UV}.x + ${Time} * 0.05, ${UV}.y)`, new Vector2(2, 1))
+		)
 
 		const color = new Color("hotpink")
 
 		const root = CustomShaderMaterialMaster({
 			diffuseColor: $`${color} * ${tex2d.color}`,
-			alpha: Fresnel()
+			alpha: Add(Fresnel(), 0.1)
 		})
 
 		return compileShader(root)
