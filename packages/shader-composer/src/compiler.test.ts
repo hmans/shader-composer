489a5a02d2b581edea8923bab26ca30584393a67
Hendrik Mans <hendrik@mans.de>
2022-07-19 11:01:33 +0200

    Render expressions

--- packages/shader-composer/src/compiler.test.ts
+++ packages/shader-composer/src/compiler.test.ts
@@ -1,4 +1,5 @@
 import { compileShader } from "./compiler"
+import { glsl } from "./expressions"
 import { Unit } from "./units"
 
 describe("compileShader", () => {
@@ -48,6 +49,32 @@ describe("compileShader", () => {
 		{
 		float Anonymous_2 = 123.0;
 		float Anonymous_1 = Anonymous_2;
+		}"
+	`)
+	})
+
+	it("resolves dependencies to expressions", () => {
+		const f = glsl`123.0`
+		const g = glsl`${f} + 4.0`
+		const root = Unit("float", glsl`${g}`)
+
+		const shader = compileShader(root)
+
+		expect(shader.vertexShader).toMatchInlineSnapshot(`
+		"/*** PROGRAM: VERTEX ***/
+
+		void main()
+		{
+		float Anonymous_1 = 123.0 + 4.0;
+		}"
+	`)
+
+		expect(shader.fragmentShader).toMatchInlineSnapshot(`
+		"/*** PROGRAM: FRAGMENT ***/
+
+		void main()
+		{
+		float Anonymous_1 = 123.0 + 4.0;
 		}"
 	`)
 	})
