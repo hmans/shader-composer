487210768e1001474cc502e3ee5e95abf40a76f5
Hendrik Mans <hendrik@mans.de>
2022-07-19 14:55:37 +0200

    Ok

--- packages/shader-composer/src/stdlib/masters.ts
+++ packages/shader-composer/src/stdlib/masters.ts
@@ -1,23 +1,36 @@
 import { Color } from "three"
-import { glsl } from "../expressions"
-import { Float, Master, Value, Vec3 } from "../units"
+import { $ } from "../expressions"
+import { Master, Value, Vec3 } from "../units"
+
+export const VertexPositionAttribute = Vec3($`position`, {
+	name: "Position",
+	only: "vertex"
+})
 
 export type CustomShaderMaterialMasterProps = {
+	position?: Value<"vec3">
 	diffuseColor?: Value<"vec3">
 	alpha?: Value<"float">
 }
 
 export const CustomShaderMaterialMaster = ({
+	position = VertexPositionAttribute,
 	diffuseColor = new Color("white"),
 	alpha = 1
 }: CustomShaderMaterialMasterProps = {}) =>
 	Master({
 		name: "CustomShaderMaterial Master",
 
+		vertex: {
+			body: $`
+				csm_Position.xyz = ${position};
+			`
+		},
+
 		fragment: {
-			body: glsl`
-				csm_DiffuseColor.rgb = ${Vec3(diffuseColor, { only: "fragment" })};
-				csm_DiffuseColor.a = ${Float(alpha, { only: "fragment" })};
+			body: $`
+				csm_DiffuseColor.rgb = ${diffuseColor};
+				csm_DiffuseColor.a = ${alpha};
 			`
 		}
 	})
