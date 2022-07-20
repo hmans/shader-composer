37bbeda3dc811ed0271fe6b4174e60c8a460d0cf
Hendrik Mans <hendrik@mans.de>
2022-07-20 13:57:53 +0200

    Get a bit of noise going

--- apps/examples/src/examples/Planet.tsx
+++ apps/examples/src/examples/Planet.tsx
@@ -1,18 +1,41 @@
-import { CustomShaderMaterialMaster } from "shader-composer"
+import {
+	Add,
+	CustomShaderMaterialMaster,
+	GLSLType,
+	Mul,
+	Simplex3DNoise,
+	Smoothstep,
+	Value,
+	VertexPosition
+} from "shader-composer"
 import { useShader } from "shader-composer-r3f"
-import { Color, MeshStandardMaterial } from "three"
+import { Color, MeshStandardMaterial, Vector3 } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 
 export default function() {
 	const shader = useShader(() => {
+		const ScaleAndOffset = <T extends GLSLType>(
+			target: Value<T>,
+			scale?: Value<T | "float">,
+			offset?: Value<T | "float">
+		) => Add(Mul(target, scale ?? 1), offset ?? 0)
+
+		const continents = Mul(
+			Smoothstep(0, 0.1, Simplex3DNoise(ScaleAndOffset(VertexPosition, 1, 10))),
+			0.1
+		)
+
+		const totalHeight = continents
+
 		return CustomShaderMaterialMaster({
-			diffuseColor: new Color("hotpink")
+			position: Mul(VertexPosition, Add(1.0, totalHeight)),
+			diffuseColor: new Color("#888")
 		})
-	})
+	}, [Math.random()])
 
 	return (
 		<mesh>
-			<icosahedronGeometry args={[1, 4]} />
+			<icosahedronGeometry args={[1, 5]} />
 			<CustomShaderMaterial baseMaterial={MeshStandardMaterial} {...shader} />
 		</mesh>
 	)
