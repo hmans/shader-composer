feff2a764070b3b076412816eee7f6649628cfed
Hendrik Mans <hendrik@mans.de>
2022-07-20 11:59:24 +0200

    Comments

--- apps/examples/src/examples/HelloWorld.tsx
+++ apps/examples/src/examples/HelloWorld.tsx
@@ -15,12 +15,14 @@ import { Color } from "three"
 export default function HelloWorld() {
 	const shader = useShader(() =>
 		ShaderMaterialMaster({
+			/* You can define pipes that will apply multiple transformations to a value. */
 			color: Pipe(
 				new Color("hotpink"),
 				(color) => Add(color, Fresnel()),
 				(color) => Add(color, Mul(new Color("green"), Sin(Time)))
 			),
 
+			/* But you can also just escape into GLSL expressions using the $ tag. */
 			position: $`${VertexPosition} * (1.0 + sin(${Time} + ${VertexPosition}.y * 2.0) * 0.2)`
 		})
 	)
