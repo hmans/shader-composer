b4dd7bf5fc4f573425ecf7d7ef918ca2cee90cb5
Hendrik Mans <hendrik@mans.de>
2022-07-20 17:07:41 +0200

    Better planet

--- apps/examples/src/examples/Planet.tsx
+++ apps/examples/src/examples/Planet.tsx
@@ -1,11 +1,18 @@
 import { pipe } from "fp-ts/lib/function"
 import {
+	$,
 	Add,
+	Clamp,
 	CustomShaderMaterialMaster,
+	Mix,
 	Mul,
 	Pow,
 	Simplex3DNoise,
+	Sin,
 	Smoothstep,
+	Step,
+	Time,
+	Vec3,
 	VertexPosition
 } from "shader-composer"
 import { useShader } from "shader-composer-r3f"
@@ -16,23 +23,35 @@ export default function() {
 	const shader = useShader(() => {
 		const continents = pipe(
 			VertexPosition,
-			(v) => Add(v, 10),
+			(v) => Add(v, Math.random() * 100),
 			(v) => Simplex3DNoise(v),
-			(v) => Smoothstep(0, 0.1, v),
+			(v) => Mul(v, 0.1)
+		)
+
+		const hills = pipe(
+			VertexPosition,
+			(v) => Mul(v, 0.8),
+			(v) => Add(v, Math.random() * 100),
+			(v) => Simplex3DNoise(v),
 			(v) => Mul(v, 0.2)
 		)
 
 		const mountains = pipe(
 			VertexPosition,
-			(v) => Mul(v, 2),
-			(v) => Add(v, 100),
+			(v) => Mul(v, 1.2),
+			(v) => Add(v, Math.random() * 100),
 			(v) => Simplex3DNoise(v),
-			(v) => Smoothstep(0, 0.1, v),
-			(v) => Pow(v, 3),
 			(v) => Mul(v, 0.2)
 		)
 
-		const totalHeight = Add(continents, mountains)
+		const totalHeight = pipe(
+			continents,
+			(v) => Add(v, hills),
+			(v) => Add(v, mountains),
+			(v) => Clamp(v, 0, 1)
+		)
+
+		const water = Add(Mul(Sin(Add(Time, $`${VertexPosition}.y`)), 0.008), 0.02)
 
 		return CustomShaderMaterialMaster({
 			position: pipe(
@@ -41,13 +60,21 @@ export default function() {
 				(v) => Mul(VertexPosition, v)
 			),
 
-			diffuseColor: new Color("#888")
+			diffuseColor: pipe(
+				Vec3(new Color("#336")),
+				(v) => Mix(v, new Color("yellow"), Step(water, totalHeight)),
+				(v) => Mix(v, new Color("#484"), Step(0.04, totalHeight)),
+				(v) => Mix(v, new Color("#262"), Step(0.1, totalHeight)),
+				(v) => Mix(v, new Color("#444"), Step(0.2, totalHeight)),
+				(v) => Mix(v, new Color("#555"), Step(0.25, totalHeight)),
+				(v) => Mix(v, new Color("#fff"), Step(0.3, totalHeight))
+			)
 		})
 	}, [Math.random()])
 
 	return (
 		<mesh>
-			<icosahedronGeometry args={[1, 5]} />
+			<icosahedronGeometry args={[1, 8]} />
 			<CustomShaderMaterial baseMaterial={MeshStandardMaterial} {...shader} />
 		</mesh>
 	)
