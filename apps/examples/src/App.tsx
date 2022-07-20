f62e85b706583c66432322db12d111d720b73821
Hendrik Mans <hendrik@mans.de>
2022-07-20 10:59:01 +0200

    Add navigation

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,12 +1,36 @@
-import { Playground } from "./examples"
-import { Venue } from "./r3f-venue/Venue"
+import { Link, useRoute } from "wouter"
+import * as examples from "./examples"
 import "./r3f-venue/styles.css"
+import { Venue } from "./r3f-venue/Venue"
+
+function Navigation() {
+	return (
+		<div style={{ position: "fixed", top: 10, left: 10, zIndex: 1 }}>
+			{Object.entries(examples).map(([name, _]) => (
+				<Link to={`/examples/${name}`} key={name}>
+					{name}
+				</Link>
+			))}
+		</div>
+	)
+}
+
+function Example() {
+	const [match, params] = useRoute("/examples/:name")
+	const name = match ? params.name : "Playground"
+	const Component = examples[name as keyof typeof examples]
+
+	return <Component />
+}
 
 function App() {
 	return (
-		<Venue>
-			<Playground />
-		</Venue>
+		<>
+			<Navigation />
+			<Venue>
+				<Example />
+			</Venue>
+		</>
 	)
 }
 
