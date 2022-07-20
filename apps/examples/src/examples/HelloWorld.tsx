7de8c1391dcb9f526e6223be237b2e6f59a25ce2
Hendrik Mans <hendrik@mans.de>
2022-07-20 12:28:24 +0200

    Improve $Op

--- apps/examples/src/examples/HelloWorld.tsx
+++ apps/examples/src/examples/HelloWorld.tsx
@@ -17,17 +17,18 @@ import { useShader } from "shader-composer-r3f"
 import { Color } from "three"
 
 const $Op = <T extends GLSLType, A extends any[]>(
-	fun: (target: Value<T>, ...args: A) => Unit<T>,
-	...args: A
-) => (target: Value<T>) => fun(target, ...args)
+	fun: (target: Value<T>, ...args: A) => Unit<T>
+) => (...args: A) => (target: Value<T>) => fun(target, ...args)
+
+const $Add = $Op(Add)
 
 export default function HelloWorld() {
 	const shader = useShader(() =>
 		ShaderMaterialMaster({
 			color: Pipe(
 				new Color("hotpink"),
-				$Op(Mix, new Color("white"), Sin(Time)),
-				$Op(Add, Fresnel())
+				$Op(Mix)(new Color("white"), Sin(Time)),
+				$Add(Fresnel())
 			),
 
 			position: $`${VertexPosition} * (1.0 + sin(${Time} + ${VertexPosition}.y * 2.0) * 0.2)`
