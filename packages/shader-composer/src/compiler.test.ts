62628bd0802b35df8254920f3e57da3b37fe20ad
Hendrik Mans <hendrik@mans.de>
2022-07-20 09:24:42 +0200

    Convert inline to normal snapshots

--- packages/shader-composer/src/compiler.test.ts
+++ packages/shader-composer/src/compiler.test.ts
@@ -4,241 +4,84 @@ import { Snippet } from "./snippets"
 import { Bool, Float, Master, Unit, Vec3 } from "./units"
 
 describe("compileShader", () => {
-	it("compiles shader programs from the given unit", () => {
-		const root = Bool(true)
-		const [shader] = compileShader(root)
+  it("compiles shader programs from the given unit", () => {
+    const root = Bool(true)
+    const [shader] = compileShader(root)
 
-		expect(shader.vertexShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: VERTEX ***/
+    expect(shader.vertexShader).toMatchSnapshot()
+    expect(shader.fragmentShader).toMatchSnapshot()
+  })
 
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  bool Anonymous_1 = true;
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
+  it("embeds body chunks in scoped blocks, with a local `value` variable", () => {
+    const root = Master({
+      fragment: {
+        body: $`gl_FragColor.rgb = vec3(1.0, 0.0, 0.0);`
+      }
+    })
 
-		expect(shader.fragmentShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: FRAGMENT ***/
+    const [shader] = compileShader(root)
+    expect(shader.fragmentShader).toMatchSnapshot()
+  })
 
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  bool Anonymous_1 = true;
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
-	})
+  it("resolves dependencies to other units", () => {
+    const f = Float(123)
+    const root = Float(f)
 
-	it("resolves dependencies to other units", () => {
-		const f = Float(123)
-		const root = Float(f)
+    const [shader] = compileShader(root)
 
-		const [shader] = compileShader(root)
+    expect(shader.vertexShader).toMatchSnapshot()
+    expect(shader.fragmentShader).toMatchSnapshot()
+  })
 
-		expect(shader.vertexShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: VERTEX ***/
+  it("resolves dependencies to other units from within expressions", () => {
+    const f = Float(123)
+    const root = Float(glsl`${f} * 2.0`)
 
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_1 = 123.0;
-		  /*** END: Anonymous ***/
-		  
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_2 = Anonymous_1;
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
+    const [shader] = compileShader(root)
 
-		expect(shader.fragmentShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: FRAGMENT ***/
+    expect(shader.vertexShader).toMatchSnapshot()
+    expect(shader.fragmentShader).toMatchSnapshot()
+  })
 
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_1 = 123.0;
-		  /*** END: Anonymous ***/
-		  
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_2 = Anonymous_1;
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
-	})
+  it("resolves dependencies to expressions", () => {
+    const a = glsl`123.0`
+    const b = glsl`${a} + 4.0`
+    const root = Float(glsl`${b}`)
 
-	it("resolves dependencies to other units from within expressions", () => {
-		const f = Float(123)
-		const root = Float(glsl`${f} * 2.0`)
+    const [shader] = compileShader(root)
 
-		const [shader] = compileShader(root)
+    expect(shader.vertexShader).toMatchSnapshot()
+    expect(shader.fragmentShader).toMatchSnapshot()
+  })
 
-		expect(shader.vertexShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: VERTEX ***/
+  it("allows units to declare themselves to exist in one program only", () => {
+    const position = Float(1, { name: "Position (Vertex Only)", only: "vertex" })
+    const color = Float(2, { name: "Color (Fragment Only)", only: "fragment" })
 
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_1 = 123.0;
-		  /*** END: Anonymous ***/
-		  
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_2 = Anonymous_1 * 2.0;
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
+    const root = Master({
+      vertex: { body: glsl`gl_Position = ${position};` },
+      fragment: { body: glsl`gl_FragColor = ${color};` }
+    })
 
-		expect(shader.fragmentShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: FRAGMENT ***/
+    const [shader] = compileShader(root)
 
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_1 = 123.0;
-		  /*** END: Anonymous ***/
-		  
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_2 = Anonymous_1 * 2.0;
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
-	})
+    expect(shader.vertexShader).toMatchSnapshot()
+    expect(shader.fragmentShader).toMatchSnapshot()
+  })
 
-	it("resolves dependencies to expressions", () => {
-		const a = glsl`123.0`
-		const b = glsl`${a} + 4.0`
-		const root = Float(glsl`${b}`)
-
-		const [shader] = compileShader(root)
-
-		expect(shader.vertexShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: VERTEX ***/
-
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_1 = 123.0 + 4.0;
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
-
-		expect(shader.fragmentShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: FRAGMENT ***/
-
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_1 = 123.0 + 4.0;
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
-	})
-
-	it("allows units to declare themselves to exist in one program only", () => {
-		const position = Float(1, { name: "Position (Vertex Only)", only: "vertex" })
-		const color = Float(2, { name: "Color (Fragment Only)", only: "fragment" })
-
-		const root = Master({
-			vertex: { body: glsl`gl_Position = ${position};` },
-			fragment: { body: glsl`gl_FragColor = ${color};` }
-		})
-
-		const [shader] = compileShader(root)
-
-		expect(shader.vertexShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: VERTEX ***/
-
-		void main()
-		{
-		  /*** BEGIN: Position (Vertex Only) ***/
-		  float Position_Vertex_Only_1 = 1.0;
-		  /*** END: Position (Vertex Only) ***/
-		  
-		  /*** BEGIN: Anonymous ***/
-		  bool Anonymous_3 = true;
-		  {
-		    bool value = Anonymous_3;
-		    gl_Position = Position_Vertex_Only_1;
-		    Anonymous_3 = value;
-		  }
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
-
-		expect(shader.fragmentShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: FRAGMENT ***/
-
-		void main()
-		{
-		  /*** BEGIN: Color (Fragment Only) ***/
-		  float Color_Fragment_Only_2 = 2.0;
-		  /*** END: Color (Fragment Only) ***/
-		  
-		  /*** BEGIN: Anonymous ***/
-		  bool Anonymous_3 = true;
-		  {
-		    bool value = Anonymous_3;
-		    gl_FragColor = Color_Fragment_Only_2;
-		    Anonymous_3 = value;
-		  }
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
-	})
-
-	it("supports snippets", () => {
-		const double = Snippet(
-			(name) => $`
+  it("supports snippets", () => {
+    const double = Snippet(
+      (name) => $`
 			float ${name}(in float a) {
 				return a * 2.0;
 			}`
-		)
+    )
 
-		const root = Float($`${double}(1.0)`)
+    const root = Float($`${double}(1.0)`)
 
-		const [shader] = compileShader(root)
+    const [shader] = compileShader(root)
 
-		expect(shader.vertexShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: VERTEX ***/
-
-
-					float snippet_c8b24f9130(in float a) {
-						return a * 2.0;
-					}
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_1 = snippet_c8b24f9130(1.0);
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
-
-		expect(shader.fragmentShader).toMatchInlineSnapshot(`
-		"/*** PROGRAM: FRAGMENT ***/
-
-
-					float snippet_c8b24f9130(in float a) {
-						return a * 2.0;
-					}
-		void main()
-		{
-		  /*** BEGIN: Anonymous ***/
-		  float Anonymous_1 = snippet_c8b24f9130(1.0);
-		  /*** END: Anonymous ***/
-		  
-		}"
-	`)
-	})
+    expect(shader.vertexShader).toMatchSnapshot()
+    expect(shader.fragmentShader).toMatchSnapshot()
+  })
 })
