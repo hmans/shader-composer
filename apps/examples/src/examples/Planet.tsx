e5960c027ff5180a7b9ab1c6866f1dfc946267a5
Hendrik Mans <hendrik@mans.de>
2022-07-20 13:31:50 +0200

    Start that planet example

--- apps/examples/src/examples/Planet.tsx
+++ apps/examples/src/examples/Planet.tsx
@@ -1,3 +1,11 @@
+import { MeshStandardMaterial } from "three"
+import CustomShaderMaterial from "three-custom-shader-material"
+
 export default function() {
-	return null
+	return (
+		<mesh>
+			<icosahedronGeometry args={[1, 4]} />
+			<CustomShaderMaterial baseMaterial={MeshStandardMaterial} />
+		</mesh>
+	)
 }
