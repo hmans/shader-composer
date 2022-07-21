3df2253877597aa1f94f254d699f005c89e37daf
Hendrik Mans <hendrik@mans.de>
2022-07-21 15:59:20 +0200

    Eh

--- apps/examples/src/examples/Water.tsx
+++ apps/examples/src/examples/Water.tsx
@@ -3,29 +3,57 @@ import {
 	Add,
 	CustomShaderMaterialMaster,
 	JoinVector2,
+	JoinVector3,
 	Mix,
+	Mul,
+	Remap,
+	SplitVector3,
 	Time,
+	Value,
 	VertexNormal,
 	VertexPosition
 } from "shader-composer"
 import { useShader } from "shader-composer-r3f"
 import { FBMNoise, GerstnerWave, ModifyVertex, PerlinNoise } from "shader-composer-toybox"
-import { Color, MeshStandardMaterial, Vector3 } from "three"
+import { Color, MeshPhysicalMaterial, MeshStandardMaterial, Vector3 } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
+
+const NormalizeNoise = (v: Value<"float">) => Remap(v, -1, 1, 0, 1)
 
 function Water() {
 	const shader = useShader(() => {
-		const diffuseColor = new Color("#66d")
+		const diffuseColor = new Color("#88c")
 
-		const { position, normal } = ModifyVertex(VertexPosition, VertexNormal, (v) =>
-			pipe(
+		const { position, normal } = ModifyVertex(VertexPosition, VertexNormal, (v) => {
+			const split = SplitVector3(v)
+
+			return pipe(
 				v,
-				(v) => Add(v, GerstnerWave(VertexPosition, JoinVector2(0, -1), 0.5, 8.0, Time)),
-				(v) => Add(v, GerstnerWave(VertexPosition, JoinVector2(0, 1), 1.25, 3, Time)),
-				(v) => Add(v, GerstnerWave(VertexPosition, JoinVector2(1, 1), 0.2, 8.0, Time)),
-				(v) => Add(v, GerstnerWave(VertexPosition, JoinVector2(1, 1), 1, 20.0, Time))
+				(v) => Add(v, Mul(GerstnerWave(v, JoinVector2(0.5, 0.5), 0.1, 10), 0.8)),
+				(v) => Add(v, Mul(GerstnerWave(v, JoinVector2(0, -0.5), 0.5, 5), 0.5)),
+				(v) => Add(v, Mul(GerstnerWave(v, JoinVector2(0.5, -0.5), 0.2, 8), 0.2)),
+				(v) => Add(v, Mul(GerstnerWave(v, JoinVector2(0.5, 1), 1, 20.0), 0.9)),
+				(v) =>
+					Add(
+						v,
+						Mul(
+							JoinVector3(0, 1.2, 0),
+							NormalizeNoise(
+								FBMNoise(JoinVector2(Add(split[0], Time), split[2]), {
+									seed: Math.random(),
+									persistance: 0.4,
+									lacunarity: 2.3,
+									scale: 0.4,
+									redistribution: 1,
+									octaves: "5",
+									terbulance: false,
+									ridge: false
+								})
+							)
+						)
+					)
 			)
-		)
+		})
 
 		return CustomShaderMaterialMaster({
 			position,
@@ -36,38 +64,17 @@ function Water() {
 
 	return (
 		<mesh position-y={-16}>
-			<boxGeometry args={[70, 16, 70, 70, 1, 70]} />
-			<CustomShaderMaterial baseMaterial={MeshStandardMaterial} {...shader} />
-		</mesh>
-	)
-}
-
-function Thingy() {
-	const shader = useShader(() => {
-		const diffuseColor = Mix<"vec3">(
-			new Color("black"),
-			new Color("white"),
-			FBMNoise(VertexPosition)
-		)
-
-		return CustomShaderMaterialMaster({
-			diffuseColor
-		})
-	})
-
-	return (
-		<mesh>
-			<icosahedronBufferGeometry args={[1.5, 8]} />
-			<CustomShaderMaterial baseMaterial={MeshStandardMaterial} {...shader} />
+			<boxGeometry args={[70, 16, 70, 100, 1, 100]} />
+			<CustomShaderMaterial
+				baseMaterial={MeshPhysicalMaterial}
+				{...shader}
+				roughness={0.2}
+				metalness={0.1}
+			/>
 		</mesh>
 	)
 }
 
 export default function WaterExample() {
-	return (
-		<>
-			<Thingy />
-			<Water />
-		</>
-	)
+	return <Water />
 }
