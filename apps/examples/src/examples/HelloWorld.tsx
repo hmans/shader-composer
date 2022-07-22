c71551384f5d2844954c62d7b1e88b1811d08ac8
Hendrik Mans <hendrik@mans.de>
2022-07-22 17:33:53 +0200

    useUniform

--- apps/examples/src/examples/HelloWorld.tsx
+++ apps/examples/src/examples/HelloWorld.tsx
@@ -1,5 +1,4 @@
 import { useControls } from "leva"
-import { useEffect, useMemo } from "react"
 import {
 	$,
 	Add,
@@ -10,29 +9,23 @@ import {
 	ShaderMaterialMaster,
 	Sin,
 	Time,
-	Uniform,
 	VertexPosition
 } from "shader-composer"
-import { useShader } from "shader-composer-r3f"
+import { useShader, useUniform } from "shader-composer-r3f"
 import { Color } from "three"
 
 export default function HelloWorld() {
 	const leva = useControls("Uniforms", { color1: "hotpink", color2: "white" })
 
-	const blackboard = useMemo(
-		() => ({
-			color1: Uniform("vec3", "u_color1", new Color(leva.color1)) /* Set initial value */,
-			color2: Uniform("vec3", "u_color2", new Color(leva.color2))
-		}),
-		[]
-	)
+	const color1 = useUniform("vec3", new Color(leva.color1))
+	const color2 = useUniform("vec3", new Color(leva.color2))
 
 	const shader = useShader(
 		() =>
 			ShaderMaterialMaster({
 				color: pipe(
-					blackboard.color1,
-					(v) => Mix(v, blackboard.color2, Remap(Sin(Time), -1, 1, 0, 1)),
+					color1,
+					(v) => Mix(v, color2, Remap(Sin(Time), -1, 1, 0, 1)),
 					(v) => Add(v, Fresnel())
 				),
 
@@ -40,16 +33,6 @@ export default function HelloWorld() {
 			}),
 		[]
 	)
-
-	useEffect(() => {
-		/* Interact with uniform value */
-		blackboard.color1.value.set(leva.color1)
-	}, [leva.color1])
-
-	useEffect(() => {
-		/* Or replace it wholesale */
-		blackboard.color2.value = new Color(leva.color2)
-	}, [leva.color2])
 
 	return (
 		<mesh>
--- packages/shader-composer-r3f/src/hooks.ts
+++ packages/shader-composer-r3f/src/hooks.ts
@@ -1,9 +1,22 @@
-import { compileShader, Unit } from "shader-composer"
-import { useMemo } from "react"
+import { compileShader, GLSLType, JSTypes, Uniform, Unit } from "shader-composer"
+import { useLayoutEffect, useMemo } from "react"
 import { useFrame } from "@react-three/fiber"
 
 export const useShader = (ctor: () => Unit, deps?: any) => {
 	const [shader, update] = useMemo(() => compileShader(ctor()), deps)
 	useFrame((_, dt) => update(dt))
 	return shader
+}
+
+export const useUniform = <T extends GLSLType>(type: T, value: JSTypes[T]) => {
+	const uniform = useMemo(() => {
+		const name = `u_${Math.floor(Math.random() * 10000)}`
+		return Uniform(type, name, value)
+	}, [])
+
+	useLayoutEffect(() => {
+		uniform.value = value
+	}, [value])
+
+	return uniform
 }
