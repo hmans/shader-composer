27f64cf3c35b47b3e9284edbbd027977fca4a84f
Hendrik Mans <hendrik@mans.de>
2022-07-22 20:11:36 +0200

    Cleanup

--- apps/examples/src/examples/HelloWorld.tsx
+++ apps/examples/src/examples/HelloWorld.tsx
@@ -15,24 +15,25 @@ import { useShader, useUniform } from "shader-composer-r3f"
 import { Color } from "three"
 
 export default function HelloWorld() {
-	const leva = useControls("Uniforms", { color1: "#bc23c2", color2: "#c7be13" })
+	const leva = useControls("Uniforms", {
+		color1: "#bc23c2",
+		color2: "#c7be13"
+	})
 
 	const color1 = useUniform("vec3", new Color(leva.color1))
 	const color2 = useUniform("vec3", new Color(leva.color2))
 
-	const shader = useShader(
-		() =>
-			ShaderMaterialMaster({
-				color: pipe(
-					color1,
-					(v) => Mix(v, color2, Remap(Sin(Time), -1, 1, 0, 1)),
-					(v) => Add(v, Fresnel())
-				),
+	const shader = useShader(() => {
+		return ShaderMaterialMaster({
+			color: pipe(
+				color1,
+				(v) => Mix(v, color2, Remap(Sin(Time), -1, 1, 0, 1)),
+				(v) => Add(v, Fresnel())
+			),
 
-				position: $`${VertexPosition} * (1.0 + sin(${Time} + ${VertexPosition}.y * 2.0) * 0.2)`
-			}),
-		[]
-	)
+			position: $`${VertexPosition} * (1.0 + sin(${Time} + ${VertexPosition}.y * 2.0) * 0.2)`
+		})
+	}, [])
 
 	return (
 		<mesh>
