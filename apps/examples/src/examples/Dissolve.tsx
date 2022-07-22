e99f000bb049ce771eb207eb46e5c8a6f7d6bf8d
Hendrik Mans <hendrik@mans.de>
2022-07-22 17:11:25 +0200

    Give the Dissolve effect a nice big upgrade

--- apps/examples/src/examples/Dissolve.tsx
+++ apps/examples/src/examples/Dissolve.tsx
@@ -4,49 +4,70 @@ import { useMemo } from "react"
 import { CustomShaderMaterialMaster, Mix, pipe, Uniform } from "shader-composer"
 import { useShader } from "shader-composer-r3f"
 import { Dissolve } from "shader-composer-toybox"
-import { Color, DoubleSide, MeshStandardMaterial } from "three"
+import {
+	Color,
+	DoubleSide,
+	MeshPhongMaterial,
+	MeshPhysicalMaterial,
+	MeshStandardMaterial
+} from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 
 export default function DissolveExample() {
-	const sphereOpts = useControls("Sphere", { color: "hotpink" })
+	const sphereOpts = useControls("Sphere", { color: "#666" })
 	const dissolveOpts = useControls("Dissolve", {
-		color: "white",
+		edgeColor: "#0ef",
+		edgeThickness: { value: 0.2, min: 0, max: 1 },
 		visibility: { value: 0.5, min: 0, max: 1 }
 	})
 
 	const blackboard = useMemo(
 		() => ({
-			sphereColor: Uniform("vec3", "u_sphereColor", new Color(sphereOpts.color)),
-			dissolveColor: Uniform("vec3", "u_dissolveColor", new Color(dissolveOpts.color)),
-			visibility: Uniform("float", "u_visibility", dissolveOpts.visibility)
+			sphere: {
+				color: Uniform("vec3", "u_sphereColor", new Color(sphereOpts.color))
+			},
+			dissolve: {
+				visibility: Uniform("float", "u_visibility", dissolveOpts.visibility),
+				edgeColor: Uniform("vec3", "u_dissolveColor", new Color(dissolveOpts.edgeColor)),
+				edgeThickness: Uniform("float", "u_edgeThickness", dissolveOpts.edgeThickness)
+			}
 		}),
 		[]
 	)
 
 	const shader = useShader(() => {
-		const dissolve = Dissolve(blackboard.visibility, 1, 0.1, blackboard.dissolveColor)
+		const dissolve = Dissolve(
+			blackboard.dissolve.visibility,
+			1,
+			blackboard.dissolve.edgeThickness
+		)
 
 		return CustomShaderMaterialMaster({
-			diffuseColor: pipe(blackboard.sphereColor, (v) => Mix(v, dissolve.color, 0.5)),
+			diffuseColor: pipe(blackboard.sphere.color, (v) =>
+				Mix(v, blackboard.dissolve.edgeColor, dissolve.edge)
+			),
 			alpha: dissolve.alpha
 		})
 	}, [])
 
 	/* TODO: Maybe shader-composer-r3f can provide some glue for this? */
 	useFrame(() => {
-		blackboard.sphereColor.value.set(sphereOpts.color)
-		blackboard.dissolveColor.value.set(dissolveOpts.color)
-		blackboard.visibility.value = dissolveOpts.visibility
+		blackboard.sphere.color.value.set(sphereOpts.color)
+		blackboard.dissolve.edgeColor.value.set(dissolveOpts.edgeColor)
+		blackboard.dissolve.edgeThickness.value = dissolveOpts.edgeThickness
+		blackboard.dissolve.visibility.value = dissolveOpts.visibility
 	})
 
 	return (
 		<mesh>
 			<icosahedronGeometry args={[2, 10]} />
 			<CustomShaderMaterial
-				baseMaterial={MeshStandardMaterial}
+				baseMaterial={MeshPhysicalMaterial}
 				{...shader}
 				transparent
 				side={DoubleSide}
+				metalness={0.1}
+				roughness={0.2}
 			/>
 		</mesh>
 	)
--- packages/shader-composer-toybox/src/effects/Dissolve.ts
+++ packages/shader-composer-toybox/src/effects/Dissolve.ts
@@ -1,6 +1,7 @@
 import {
-	Add,
+	Float,
 	Mul,
+	pipe,
 	Remap,
 	Smoothstep,
 	Step,
@@ -8,25 +9,34 @@ import {
 	Value,
 	VertexPosition
 } from "shader-composer"
-import { Color } from "three"
 import { Simplex3DNoise } from "../noise/Simplex3DNoise"
 
 export const Dissolve = (
 	visibility: Value<"float"> = 0.5,
 	scale: Value<"float"> = 1,
 	edgeThickness: Value<"float"> = 0.1,
-	edgeColor: Value<"vec3"> = new Color(0, 10, 8)
+	varying = false
 ) => {
-	const noise = Remap(
-		Simplex3DNoise(Mul(VertexPosition, scale)),
-		-1,
-		Add(edgeThickness, 1),
-		0,
-		1
+	const noise = pipe(
+		VertexPosition,
+
+		/* Scale vertex position */
+		(v) => Mul(v, scale),
+
+		/* Make some noise */
+		(v) => Simplex3DNoise(v),
+
+		/* Remap to 0-1, taking edge thickness into account */
+		(v) => Remap(v, -1, 1, 0, Sub(1, edgeThickness)),
+
+		/* Wrap resulting value in a float unit that _may_ use a varying */
+		(v) => Float(v, { varying })
 	)
 
+	const edge = Smoothstep(Sub(visibility, edgeThickness), visibility, noise)
+
 	return {
-		color: Mul(edgeColor, Smoothstep(Sub(visibility, edgeThickness), visibility, noise)),
+		edge,
 		alpha: Step(noise, visibility)
 	}
 }
