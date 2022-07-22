e7d16ba889eeb862655bb11775097829920e9c3d
Hendrik Mans <hendrik@mans.de>
2022-07-22 13:00:34 +0200

    Try an outer Suspense

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from "react"
 import examples from "./examples"
 import "./r3f-venue/styles.css"
 import { Venue } from "./r3f-venue/Venue"
 
 function App() {
-	return <Venue examples={examples} />
+	return (
+		<Suspense fallback={<p>Loading...</p>}>
+			<Venue examples={examples} />
+		</Suspense>
+	)
 }
 
 export default App
