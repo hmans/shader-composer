6fdc3142fffa59f6f828dc97c99b6aaa8e4b9b22
Hendrik Mans <hendrik@mans.de>
2022-07-28 12:15:10 +0200

    Add new collectors module

---
+++ packages/shader-composer/src/collector.ts
@@ -0,0 +1,22 @@
+import { Expression } from "./expressions"
+import { Snippet } from "./snippets"
+import { isUnit, Program, Unit } from "./units"
+
+type Item = Unit | Expression | Snippet
+
+export const collectItems = (
+	item: Item,
+	program: Program,
+	state = { items: new Set<Item>() }
+) => {
+	/* Check if we've already collected this item */
+	if (state.items.has(item)) return
+	state.items.add(item)
+
+	/* Collect dependencies */
+	if (isUnit(item)) {
+	}
+
+	/* Add the given item */
+	state.items.add(item)
+}
