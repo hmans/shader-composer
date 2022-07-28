16328b83a515c9f763266b48fc472a2ff0594f92
Hendrik Mans <hendrik@mans.de>
2022-07-28 22:33:22 +0200

    Set up new example

---
+++ apps/examples/src/examples/MatrixTransformations.tsx
@@ -0,0 +1,22 @@
+import { CustomShaderMaterialMaster, Time } from "shader-composer"
+import { useShader } from "shader-composer-r3f"
+import { MeshStandardMaterial } from "three"
+import CustomShaderMaterial from "three-custom-shader-material"
+
+export default function MatrixTransformations() {
+	const shader = useShader(() => {
+		const time = Time()
+		return CustomShaderMaterialMaster({})
+	}, [])
+
+	return (
+		<mesh>
+			<sphereGeometry />
+			<CustomShaderMaterial
+				baseMaterial={MeshStandardMaterial}
+				color="orange"
+				{...shader}
+			/>
+		</mesh>
+	)
+}
