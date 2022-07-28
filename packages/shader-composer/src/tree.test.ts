eca410b45243cd242133a1267658004843010a1d
Hendrik Mans <hendrik@mans.de>
2022-07-28 14:09:28 +0200

    Test

---
+++ packages/shader-composer/src/tree.test.ts
@@ -0,0 +1,18 @@
+import { $ } from "./expressions"
+import { Snippet } from "./snippets"
+import { Item, walkTree } from "./tree"
+import { Float } from "./units"
+
+describe("walkTree", () => {
+	it("walks a given node tree", () => {
+		const a = Float(1)
+		const snippet = Snippet(() => $`${a}`)
+		const expr = $`${snippet}`
+		const root = Float(expr)
+		const seen = new Array<Item>()
+
+		walkTree(root, "vertex", (item) => seen.push(item))
+
+		expect(seen).toEqual([a, snippet, expr, root])
+	})
+})
