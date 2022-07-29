17e24e954ea39b3793e24a3c9f53617acc1d22f5
Hendrik Mans <hendrik@mans.de>
2022-07-29 11:02:09 +0200

    Add more rotation functions

--- packages/shader-composer/src/vendor/glsl-rotate.ts
+++ packages/shader-composer/src/vendor/glsl-rotate.ts
@@ -26,20 +26,66 @@ IN THE SOFTWARE.
 
 */
 
-import { glsl } from "../expressions"
+import { $, glsl } from "../expressions"
 import { Snippet } from "../snippets"
 
 export const rotation3d = Snippet(
-	(rotation3d) => glsl`mat4 ${rotation3d}(vec3 axis, float angle) {
-  axis = normalize(axis);
-  float s = sin(angle);
-  float c = cos(angle);
-  float oc = 1.0 - c;
-  return mat4(
-    oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
-    oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
-    oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
-    0.0,                                0.0,                                0.0,                                1.0
-  );
-}`
+	(name) => glsl`
+		mat4 ${name}(vec3 axis, float angle) {
+			axis = normalize(axis);
+			float s = sin(angle);
+			float c = cos(angle);
+			float oc = 1.0 - c;
+			return mat4(
+				oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+				oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+				oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+				0.0,                                0.0,                                0.0,                                1.0
+			);
+		}
+	`
+)
+
+export const rotation3dX = Snippet(
+	(name) => glsl`
+		mat3 ${name}(float angle) {
+			float s = sin(angle);
+			float c = cos(angle);
+			return mat3(
+				1.0, 0.0, 0.0,
+				0.0, c, s,
+				0.0, -s, c
+			);
+		}
+	`
+)
+
+export const rotation3dY = Snippet(
+	(name) => glsl`
+		mat3 ${name}(float angle) {
+			float s = sin(angle);
+			float c = cos(angle);
+		
+			return mat3(
+				c, 0.0, -s,
+				0.0, 1.0, 0.0,
+				s, 0.0, c
+			);
+		}
+	`
+)
+
+export const rotation3dZ = Snippet(
+	(name) => glsl`
+		mat3 ${name}(float angle) {
+			float s = sin(angle);
+			float c = cos(angle);
+		
+			return mat3(
+				c, s, 0.0,
+				-s, c, 0.0,
+				0.0, 0.0, 1.0
+			);
+		}
+	`
 )
