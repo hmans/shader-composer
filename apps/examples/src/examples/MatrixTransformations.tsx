090d9b2567d71dab1e37a632e1611adbe6d73ead
Hendrik Mans <hendrik@mans.de>
2022-07-29 10:13:50 +0200

    Rotate quat

--- apps/examples/src/examples/MatrixTransformations.tsx
+++ apps/examples/src/examples/MatrixTransformations.tsx
@@ -1,14 +1,27 @@
-import { CustomShaderMaterialMaster, Time, VertexPosition } from "shader-composer"
+import {
+	$,
+	CustomShaderMaterialMaster,
+	Time,
+	Value,
+	Vec3,
+	VertexPosition
+} from "shader-composer"
 import { useShader } from "shader-composer-r3f"
-import { MeshStandardMaterial } from "three"
+import { MeshStandardMaterial, Vector4 } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 
 export default function MatrixTransformations() {
 	const shader = useShader(() => {
 		const time = Time()
 
+		const StupidRotation = (v: Value<"vec3">, q: Value<"vec4">) =>
+			Vec3($`
+				${v} 
+					+ 2.0 * cross(${q}.xyz, cross(${q}.xyz, ${v})
+					+ ${q}.w * ${v})`)
+
 		return CustomShaderMaterialMaster({
-			position: VertexPosition
+			position: StupidRotation(VertexPosition, new Vector4(0, 0, 0, 1))
 		})
 	}, [])
 
