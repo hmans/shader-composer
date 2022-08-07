94af8ed3f33c345b00a7262bc7756b39046c9da2
Hendrik Mans <hendrik@mans.de>
2022-08-07 10:12:01 +0200

    Remove `normalize`, `cross`, `dot` expressions

--- packages/shader-composer/src/stdlib/vectors.ts
+++ packages/shader-composer/src/stdlib/vectors.ts
@@ -24,23 +24,16 @@ export const orthogonal = (v: Input<"vec3">) => $/*glsl*/ `
     : vec3( 0.0, -${v}.z, ${v}.y)
   )`
 
-export const normalize = (a: Input) => $`normalize(${a})`
-
 export const Normalize = <T extends "vec2" | "vec3" | "vec4">(a: Input<T>) =>
-	Unit(type(a) as T, normalize(a), { name: "Normalize" })
-
-export const cross = (a: Input<"vec3">, b: Input<"vec3">) => $`cross(${a}, ${b})`
+	Unit(type(a) as T, $`normalize(${a})`, { name: "Normalize" })
 
 export const Cross = (a: Input<"vec3">, b: Input<"vec3">) =>
-	Vec3(cross(a, b), { name: "Cross Product" })
-
-export const dot = <T extends "vec2" | "vec3" | "vec4">(a: Input<T>, b: Input<T>) =>
-	$`dot(${a}, ${b})`
+	Vec3($`cross(${a}, ${b})`, { name: "Cross Product" })
 
 export const Dot = <T extends "vec2" | "vec3" | "vec4">(a: Input<T>, b: Input<T>) =>
-	Float(dot(a, b), { name: "Dot Product" })
+	Float($`dot(${a}, ${b})`, { name: "Dot Product" })
 
 export const Tangent = (v: Input<"vec3">) => Vec3(orthogonal(v), { name: "Tangent" })
 
 export const Bitangent = (p: Input<"vec3">, t: Input<"vec3">) =>
-	Vec3(normalize(cross(p, t)), { name: "Bitangent" })
+	Vec3(Normalize(Cross(p, t)), { name: "Bitangent" })
