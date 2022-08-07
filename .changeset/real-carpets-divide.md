298f5f1b9e8447202e7f945b89fa8bb2c3cc1e73
Hendrik Mans <hendrik@mans.de>
2022-08-07 08:23:32 +0200

    Rotate3D, RotateX, RotateY, RotateZ

---
+++ .changeset/real-carpets-divide.md
@@ -0,0 +1,5 @@
+---
+"shader-composer": patch
+---
+
+**Added:** `Rotate3DX`, `Rotate3DY`, `Rotate3DZ` and `Rotate3D`, units that will immediately rotate a given vector (unlike `Rotation3DX`, `Rotation3D` etc., which just generate matrices representing the rotations.)
--- apps/examples/src/examples/Rotation.tsx
+++ apps/examples/src/examples/Rotation.tsx
@@ -1,26 +1,17 @@
-import { Input } from "shader-composer"
 import { useShader } from "shader-composer-r3f"
 import {
 	CustomShaderMaterialMaster,
-	mat3,
-	Mul,
-	Rotation3D,
+	Rotate3D,
 	Time,
 	VertexPosition
 } from "shader-composer/stdlib"
 import { MeshStandardMaterial, Vector3 } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 
-const RotateAroundAxis = (
-	position: Input<"vec3">,
-	axis: Input<"vec3">,
-	angle: Input<"float">
-) => Mul(position, mat3(Rotation3D(axis, angle)))
-
 export default function MatrixTransformations() {
 	const shader = useShader(() => {
 		return CustomShaderMaterialMaster({
-			position: RotateAroundAxis(VertexPosition, new Vector3(0.5, 1, 0), Time())
+			position: Rotate3D(VertexPosition, new Vector3(0.5, 1, 0), Time())
 		})
 	}, [])
 
