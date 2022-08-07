61c01fa2f7a539054cc30dafa88dedd96ea72e40
Hendrik Mans <hendrik@mans.de>
2022-08-07 08:58:53 +0200

    Tweak Texture2D

--- packages/shader-composer/src/stdlib/textures.ts
+++ packages/shader-composer/src/stdlib/textures.ts
@@ -1,16 +1,15 @@
 import { $ } from "../expressions"
 import { Input, Unit } from "../units"
-import { bless } from "../util/bless"
 import { Float, Vec3, Vec4 } from "./values"
 
-export const Texture2D = (sampler2D: Unit<"sampler2D">, xy: Input<"vec2">) =>
-	bless(
-		Vec4($`texture2D(${sampler2D}, ${xy})`, {
-			name: "Texture2D"
-		}),
+export const Texture2D = (sampler2D: Unit<"sampler2D">, xy: Input<"vec2">) => ({
+	...Vec4($`texture2D(${sampler2D}, ${xy})`, {
+		name: "Texture2D"
+	}),
 
-		(unit) => ({
-			color: Vec3($`${unit}.rgb`),
-			alpha: Float($`${unit}.a`)
-		})
-	)
+	/** The color value sampled from the texture. */
+	color: Vec3($`texture2D(${sampler2D}, ${xy}).rgb`, { name: "Texture2D Color" }),
+
+	/** The alpha value sampled from the texture. */
+	alpha: Float($`texture2D(${sampler2D}, ${xy}).a`, { name: "Texture2D Alpha" })
+})
