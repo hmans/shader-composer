41c6940df3f20557e3d5973947f323f3e911f667
Hendrik Mans <hendrik@mans.de>
2022-08-07 14:54:44 +0200

    Simplify snippets

--- packages/shader-composer/src/snippets.ts
+++ packages/shader-composer/src/snippets.ts
@@ -1,26 +1,27 @@
-import sha256 from "crypto-js/sha256"
 import { Expression } from "./expressions"
 
 export type Snippet = {
 	_: "Snippet"
 	name: string
+	render: (name: string) => Expression
 	expression: Expression
 }
 
-export const Snippet = (fun: (name: string) => Expression): Snippet => {
-	/* Calculate a hash, based on the expression's contents */
-	const hash = sha256(fun("").render())
-		.toString()
-		.substring(0, 10)
-
-	/* Build a name using the hash */
-	const name = `snippet_${hash}`
+export const Snippet = (render: (name: string) => Expression): Snippet => {
+	/* Start with a randomized name. The compiler will assign a deterministic name in its prepare step. */
+	const name = `uninitialized_snippet_${Math.floor(Math.random() * 1000000)}`
 
 	return {
 		_: "Snippet",
 		name,
-		expression: fun(name)
+		render,
+		expression: render(name)
 	}
+}
+
+export const renameSnippet = (snippet: Snippet, name: string) => {
+	snippet.name = name
+	snippet.expression = snippet.render(name)
 }
 
 export function isSnippet(x: any): x is Snippet {
