938e40acfb6c8d74d5f1a0053e4169ba81669e0b
Hendrik Mans <hendrik@mans.de>
2022-08-07 08:49:03 +0200

    Use bless for Texture2D

--- packages/shader-composer/src/stdlib/textures.ts
+++ packages/shader-composer/src/stdlib/textures.ts
@@ -1,15 +1,16 @@
 import { $ } from "../expressions"
 import { Input, Unit } from "../units"
+import { bless } from "../util/bless"
 import { Float, Vec3, Vec4 } from "./values"
 
-export const Texture2D = (sampler2D: Unit<"sampler2D">, xy: Input<"vec2">) => {
-	const unit = Vec4($`texture2D(${sampler2D}, ${xy})`, {
-		name: "Texture2D"
-	})
+export const Texture2D = (sampler2D: Unit<"sampler2D">, xy: Input<"vec2">) =>
+	bless(
+		Vec4($`texture2D(${sampler2D}, ${xy})`, {
+			name: "Texture2D"
+		}),
 
-	return {
-		...unit,
-		color: Vec3($`${unit}.rgb`),
-		alpha: Float($`${unit}.a`)
-	}
-}
+		(unit) => ({
+			color: Vec3($`${unit}.rgb`),
+			alpha: Float($`${unit}.a`)
+		})
+	)
