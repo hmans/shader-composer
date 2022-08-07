d8ae4f64fb0be3f8c87ab7c56ba06942e00037ab
Hendrik Mans <hendrik@mans.de>
2022-08-07 08:47:40 +0200

    Tweak bless

--- packages/shader-composer/src/util/bless.ts
+++ packages/shader-composer/src/util/bless.ts
@@ -1,3 +1,12 @@
-export function bless<O, A>(object: O, api: (o: O) => A): O & A {
-	return Object.assign(object, api(object))
+import { Unit } from "../units"
+
+/**
+ * Blesses the given unit with an API.
+ *
+ * @param unit The unit to bless.
+ * @param api A constructor function that returns an API for the unit.
+ * @returns A blessed version of the specified version.
+ */
+export function bless<U extends Unit, A>(unit: U, api: (o: U) => A): U & A {
+	return Object.assign(unit, api(unit))
 }
