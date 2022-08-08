362bd4a79439c6725a6dd50cbe1354f7106adcb7
Hendrik Mans <hendrik@mans.de>
2022-08-08 11:03:55 +0200

    Remove Math.random from Hello World

--- apps/examples/src/examples/HelloWorld.tsx
+++ apps/examples/src/examples/HelloWorld.tsx
@@ -41,7 +41,7 @@ export default function HelloWorld() {
 	return (
 		<mesh>
 			<sphereGeometry />
-			<shaderMaterial {...shader} key={Math.random()} />
+			<shaderMaterial {...shader} />
 		</mesh>
 	)
 }
