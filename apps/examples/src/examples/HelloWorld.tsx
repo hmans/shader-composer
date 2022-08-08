687b487bcd949f0f13beb2ea1572e74349f37cdc
Hendrik Mans <hendrik@mans.de>
2022-08-08 11:06:33 +0200

    Comments

--- apps/examples/src/examples/HelloWorld.tsx
+++ apps/examples/src/examples/HelloWorld.tsx
@@ -16,24 +16,33 @@ import { useShader, useUniform } from "shader-composer-r3f"
 import { Color } from "three"
 
 export default function HelloWorld() {
+	/* We're using Leva to let the user control the colors. */
 	const leva = useControls("Uniforms", {
 		color1: "#bc23c2",
 		color2: "#c7be13"
 	})
 
+	/* Here we're creating two uniforms and feeding the
+	user-provided colors into them. */
 	const color1 = useUniform("vec3", new Color(leva.color1))
 	const color2 = useUniform("vec3", new Color(leva.color2))
 
+	/* Let's create the shader itself! */
 	const shader = useShader(() => {
+		/* Create a time unit, always useful! */
 		const time = Time()
 
 		return ShaderMaterialMaster({
+			/* Set the color as a time-based mix between the two colors. */
 			color: pipe(
 				color1,
 				(v) => Mix(v, color2, NormalizePlusMinusOne(Sin(time))),
 				(v) => Add(v, Fresnel())
 			),
 
+			/* Also do a little vertex displacement. Just for fun, we'll use
+			a GLSL expression here. They're pretty cool, because their dependencies
+			are still part of the shader graph! */
 			position: $`${VertexPosition} * (1.0 + sin(${time} + ${VertexPosition}.y * 2.0) * 0.2)`
 		})
 	}, [])
@@ -41,6 +50,8 @@ export default function HelloWorld() {
 	return (
 		<mesh>
 			<sphereGeometry />
+
+			{/* We can just splat the complete shader object into shaderMaterial: */}
 			<shaderMaterial {...shader} />
 		</mesh>
 	)
