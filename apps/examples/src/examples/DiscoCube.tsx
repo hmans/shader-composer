e0a30fcd6c26345506fa1c8db16cc03923617319
Hendrik Mans <hendrik@mans.de>
2022-08-08 18:07:27 +0200

    New DiscoCube example (#83)

---
+++ apps/examples/src/examples/DiscoCube.tsx
@@ -0,0 +1,64 @@
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+import {
+  Add,
+  CustomShaderMaterialMaster,
+  Div,
+  Floor,
+  Fract,
+  Mul,
+  NormalizePlusMinusOne,
+  OneMinus,
+  pipe,
+  Sub,
+  Time,
+  VertexPosition
+} from "shader-composer"
+import { useShader } from "shader-composer-r3f"
+import { PSRDNoise3D } from "shader-composer-toybox"
+import { Color, Mesh, MeshPhysicalMaterial } from "three"
+import CustomShaderMaterial from "three-custom-shader-material"
+
+export default function DiscoCube() {
+  const mesh = useRef<Mesh>(null!)
+
+  const shader = useShader(() => {
+    const time = Time()
+    const resolution = 0.25
+
+    const noise = pipe(
+      VertexPosition,
+      (v) => Add(v, 100),
+      (v) => Div(v, resolution),
+      (v) => Floor(v),
+      (v) => Mul(v, resolution),
+      (v) => PSRDNoise3D(v),
+      (v) => NormalizePlusMinusOne(v),
+      (v) => Sub(v, Mul(time, 0.1)),
+      (v) => Fract(v)
+    )
+
+    return CustomShaderMaterialMaster({
+      diffuseColor: Mul(new Color("#abf"), noise),
+      metalness: noise,
+      roughness: OneMinus(noise)
+    })
+  }, [])
+
+  useFrame((_, dt) => {
+    mesh.current.rotation.x += dt * 0.1
+    mesh.current.rotation.y += dt * 0.5
+    mesh.current.rotation.z += dt * 0.3
+  })
+
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry args={[1.5, 1.5, 1.5]} />
+      <CustomShaderMaterial
+        baseMaterial={MeshPhysicalMaterial}
+        clearcoat={0.2}
+        {...shader}
+      />
+    </mesh>
+  )
+}
--- apps/examples/src/examples/index.tsx
+++ apps/examples/src/examples/index.tsx
@@ -1,12 +1,13 @@
 import { lazy } from "react"
 
 export default {
-	HelloWorld: lazy(() => import("./HelloWorld")),
-	Rotation: lazy(() => import("./Rotation")),
-	Flag: lazy(() => import("./Flag")),
-	Dissolve: lazy(() => import("./Dissolve")),
-	Fireball: lazy(() => import("./Fireball")),
-	Textures: lazy(() => import("./Textures")),
-	Planet: lazy(() => import("./Planet")),
-	Water: lazy(() => import("./Water"))
+  HelloWorld: lazy(() => import("./HelloWorld")),
+  Rotation: lazy(() => import("./Rotation")),
+  Flag: lazy(() => import("./Flag")),
+  Dissolve: lazy(() => import("./Dissolve")),
+  Fireball: lazy(() => import("./Fireball")),
+  Textures: lazy(() => import("./Textures")),
+  Planet: lazy(() => import("./Planet")),
+  DiscoCube: lazy(() => import("./DiscoCube")),
+  Water: lazy(() => import("./Water"))
 }
