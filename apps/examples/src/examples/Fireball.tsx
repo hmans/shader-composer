ab13b1be50c9c98eae9877a5295d75273a599cbf
Hendrik Mans <hendrik@mans.de>
2022-08-08 08:06:43 +0200

    Better Turbulence implementation

--- apps/examples/src/examples/Fireball.tsx
+++ apps/examples/src/examples/Fireball.tsx
@@ -26,40 +26,27 @@ import { MeshStandardMaterial } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 import textureUrl from "./textures/explosion.png"
 
-const IdiomaticTurbulence = (
-	p: Input<"vec3">,
-	octaves: Input<"float"> = 10,
-	noiseUnit: (p: Input<"vec3">) => Input<"float"> = Simplex3DNoise
-) => {
-	let value = Float(-0.5)
+const turbulence = (noiseFun: Snippet) =>
+	Snippet(
+		(turbulence) => $`
+			float ${turbulence}(vec3 p, float octaves) {
+				float t = -0.5;
+					
+				for (float f = 1.0 ; f <= octaves; f++) {
+					float power = pow(2.0, f);
+					t += abs(${noiseFun}(vec3(power * p)) / power);
+				}
 
-	for (let f = 0; f <= octaves; f++) {
-		const power = Math.pow(2, f)
-		value = Add(value, Abs(Div(noiseUnit(Mul(p, power)), power)))
-	}
-
-	return value
-}
+				return t;
+			}
+		`
+	)
 
 const Turbulence = (
 	p: Input<"vec3">,
 	octaves: Input<"float"> = 10,
 	noiseFun: Snippet = simplex3Dnoise
-) => {
-	const body = $`
-		value = -0.5;
-				
-		for (float f = 1.0 ; f <= ${octaves}; f++) {
-			float power = pow(2.0, f);
-			value += abs(${noiseFun}(vec3(power * ${p})) / power);
-		}
-	`
-
-	return Float(1, {
-		fragment: { body },
-		vertex: { body }
-	})
-}
+) => Float($`${turbulence(noiseFun)}(${p}, ${octaves})`, { name: `Turbulence` })
 
 export default function Fireball() {
 	const texture = useTexture(textureUrl)
@@ -84,7 +71,7 @@ export default function Fireball() {
 			(v) => vec3(Mul(v, 0.6), Mul(v, 0.8), 0),
 			(v) => Add(VertexPosition, v),
 			(v) => Mul(v, 0.25),
-			(v) => IdiomaticTurbulence(v),
+			(v) => Turbulence(v),
 			(v) => NormalizePlusMinusOne(v),
 			(v) => Pow(v, 0.5)
 		)
