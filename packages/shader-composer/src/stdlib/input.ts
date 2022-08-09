4bb854afcb0eb06c4cc1660aaf27c58d8eedab17
Hendrik Mans <hendrik@mans.de>
2022-08-09 10:56:58 +0200

    New uniforms et al

--- packages/shader-composer/src/stdlib/input.ts
+++ packages/shader-composer/src/stdlib/input.ts
@@ -1,57 +1,70 @@
-import { Vector2 } from "three"
+import { Camera, PerspectiveCamera, Vector2 } from "three"
 import { GLSLType, JSTypes, Unit, UnitConfig } from "../units"
 import { Float } from "./values"
 
 export const uniformName = (unit: Unit) =>
-	unit._unitConfig.uniformName ?? `u_${unit._unitConfig.variableName}`
+  unit._unitConfig.uniformName ?? `u_${unit._unitConfig.variableName}`
 
 export const Uniform = <T extends GLSLType, U extends JSTypes[T]>(
-	type: T,
-	initialValue: U,
-	extras?: Partial<UnitConfig<T>>
+  type: T,
+  initialValue: U,
+  extras?: Partial<UnitConfig<T>>
 ) => {
-	/*
+  /*
 	Create a uniform object. One of the reasons we need to wrap the value here
 	is that there is a good chance that we will need to mutate it, and we can only
 	reliably do this for scalar values if they are wrapped.
 	*/
-	const uniform = { value: initialValue }
+  const uniform = { value: initialValue }
 
-	/* Create the actual unit that represents the uniform. */
-	const unit = Unit<T>(type, undefined, {
-		name: `Uniform (${type})`,
-		...extras,
-		uniform,
-		variable: false
-	})
+  /* Create the actual unit that represents the uniform. */
+  const unit = Unit<T>(type, undefined, {
+    name: `Uniform (${type})`,
+    ...extras,
+    uniform,
+    variable: false
+  })
 
-	/* Return the unit with some API bits mixed in. */
-	return {
-		...unit,
+  /* Return the unit with some API bits mixed in. */
+  return {
+    ...unit,
 
-		toString: () => uniformName(unit),
+    toString: () => uniformName(unit),
 
-		/** The uniform's value. */
-		set value(v: U) {
-			uniform.value = v
-		},
+    /** The uniform's value. */
+    set value(v: U) {
+      uniform.value = v
+    },
 
-		get value(): U {
-			return uniform.value
-		}
-	}
+    get value(): U {
+      return uniform.value
+    }
+  }
 }
 
 export const Time = (initial: number = 0) => {
-	const uniform = Uniform("float", initial, { name: "Time Uniform" })
+  const uniform = Uniform("float", initial, { name: "Time Uniform" })
 
-	return {
-		...Float(uniform, {
-			name: "Time",
-			update: (dt) => (uniform.value += dt)
-		}),
-		uniform
-	}
+  return {
+    ...Float(uniform, {
+      name: "Time",
+      update: (dt) => (uniform.value += dt)
+    }),
+    uniform
+  }
 }
 
 export const Resolution = Uniform("vec2", new Vector2(0, 0))
+export const CameraNear = Uniform("float", 0 as number)
+export const CameraFar = Uniform("float", 1000 as number)
+
+/**
+ * Updates the `CameraNear` and `CameraFar` uniforms with the information from
+ * the given camera instance.
+ */
+export const updateCameraUniforms = (camera: Camera) => {
+  if (camera instanceof PerspectiveCamera) {
+    CameraNear.value = camera.near
+    CameraFar.value = camera.far
+  }
+}
