bbf8b10fa1d76c0abc7fe9abad355a497fcfe818
Hendrik Mans <hendrik@mans.de>
2022-08-09 12:01:38 +0200

    Set up a "level"

--- apps/examples/src/examples/Shield.tsx
+++ apps/examples/src/examples/Shield.tsx
@@ -9,9 +9,32 @@ export default function Shield() {
   })
 
   return (
-    <mesh>
-      <icosahedronGeometry args={[1, 3]} />
-      <CustomShaderMaterial baseMaterial={MeshStandardMaterial} {...shader} transparent />
-    </mesh>
+    <group>
+      <mesh position={[0, -0.5, -0.5]}>
+        <icosahedronGeometry args={[1.3, 3]} />
+        <CustomShaderMaterial
+          baseMaterial={MeshStandardMaterial}
+          {...shader}
+          transparent
+        />
+      </mesh>
+
+      <Floor />
+      <Wall />
+    </group>
   )
 }
+
+const Floor = () => (
+  <mesh position={[0, -1.5, 0]}>
+    <boxGeometry args={[4, 1, 4]} />
+    <meshStandardMaterial color="#555" />
+  </mesh>
+)
+
+const Wall = () => (
+  <mesh position={[0, 0, -1.5]}>
+    <boxGeometry args={[4, 4, 1]} />
+    <meshStandardMaterial color="#555" />
+  </mesh>
+)
