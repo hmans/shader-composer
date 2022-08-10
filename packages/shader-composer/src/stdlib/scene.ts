8ef1bd540e2dd58192293e1a27b0df8019739d58
Hendrik Mans <hendrik@mans.de>
2022-08-10 18:30:48 +0200

    Raw

--- packages/shader-composer/src/stdlib/scene.ts
+++ packages/shader-composer/src/stdlib/scene.ts
@@ -6,6 +6,25 @@ import { CameraFar, CameraNear, Uniform } from "./input"
 import { Texture2D } from "./textures"
 import { Float } from "./values"
 
+/**
+ * Sample a depth texture and return the raw depth value.
+ *
+ * @param uv Screen UV
+ * @param depthTexture Depth texture to sample
+ * @returns Float unit containing the depth as stored in the texture
+ */
+export const RawDepth = (uv: Input<"vec2">, depthTexture: Unit<"sampler2D">) =>
+  Float(Texture2D(depthTexture, uv).x, { name: "Read Depth from Depth Texture (Raw)" })
+
+/**
+ * Sample a depth texture and return the depth value in eye space units.
+ *
+ * @param xy Screen position
+ * @param depthTexture Depth texture to sample
+ * @param cameraNear Camera near plane (defaults to CameraNear)
+ * @param cameraFar Camera far plane (defaults to CameraFar)
+ * @returns Float unit containing the depth in eye space units
+ */
 export const PerspectiveDepth = (
   xy: Input<"vec2">,
   depthTexture: Unit<"sampler2D">,
@@ -15,10 +34,10 @@ export const PerspectiveDepth = (
   Float(
     pipe(
       xy,
-      (v) => Texture2D(depthTexture, v).x,
+      (v) => RawDepth(v, depthTexture),
       (v) => $`perspectiveDepthToViewZ(${v}, ${cameraNear}, ${cameraFar})`
     ),
-    { name: "Read Depth from Depth Texture" }
+    { name: "Read Depth from Depth Texture (Eye Space)" }
   )
 
 export type SceneDepthOptions = {
