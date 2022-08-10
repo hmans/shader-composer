3f1aaea136628d65a3f75fa8439e5174aec90184
Hendrik Mans <hendrik@mans.de>
2022-08-10 10:01:36 +0200

    Simplify

--- packages/shader-composer/src/stdlib/spaces.ts
+++ packages/shader-composer/src/stdlib/spaces.ts
@@ -2,29 +2,26 @@ import { $ } from "../expressions"
 import { type } from "../glslType"
 import { Input } from "../units"
 import { InstanceMatrix, ModelMatrix, ModelViewMatrix } from "./geometry"
-import { Vec4 } from "./values"
+import { Vec3, Vec4 } from "./values"
 
-const castToVec4 = (v: Input<"vec3" | "vec4">) =>
-  type(v) === "vec4" ? v : $`vec4(${v}, 1.0)`
-
-export const localToViewSpace = (v: Input<"vec3" | "vec4">) => $`
-  (${InstanceMatrix} * ${ModelViewMatrix} * ${castToVec4(v)})
+export const localToViewSpace = (v: Input<"vec3">) => $`
+  vec3(${InstanceMatrix} * ${ModelViewMatrix} * vec4(${v}, 1.0))
 `
 
-export const localToWorldSpace = (v: Input<"vec3" | "vec4">) => $`
-  (${InstanceMatrix} * ${ModelMatrix} * ${castToVec4(v)})
+export const localToWorldSpace = (v: Input<"vec3">) => $`
+  vec3(${InstanceMatrix} * ${ModelMatrix} * vec4(${v}, 1.0))
 `
 
 /**
  * Converts the given position vector (which is assumed to be in local space)
  * to view space.
  */
-export const LocalToViewSpace = (position: Input<"vec3" | "vec4">) =>
-  Vec4(localToViewSpace(position))
+export const LocalToViewSpace = (position: Input<"vec3">) =>
+  Vec3(localToViewSpace(position))
 
 /**
  * Converts the given position vector (which is assumed to be in local space)
  * to world space.
  */
-export const LocalToWorldSpace = (position: Input<"vec3" | "vec4">) =>
-  Vec4(localToWorldSpace(position))
+export const LocalToWorldSpace = (position: Input<"vec3">) =>
+  Vec3(localToWorldSpace(position))
