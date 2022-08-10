fb3da3c9caa39d3255f1209c0a965c8c5cc0c41f
Hendrik Mans <hendrik@mans.de>
2022-08-10 08:56:30 +0200

    castToVec4

--- packages/shader-composer/src/stdlib/spaces.ts
+++ packages/shader-composer/src/stdlib/spaces.ts
@@ -1,18 +1,22 @@
 import { $ } from "../expressions"
+import { type } from "../glslType"
 import { Input } from "../units"
 import { Vec4 } from "./values"
+
+const castToVec4 = (v: Input<"vec3" | "vec4">) =>
+  type(v) === "vec4" ? v : $`vec4(${v}, 1.0)`
 
 /**
  * Converts the given position vector (which is assumed to be in local space)
  * to view space.
  */
-export const ToViewSpace = (position: Input<"vec3">) =>
+export const ToViewSpace = (position: Input<"vec3" | "vec4">) =>
   Vec4(
     $`
       #ifdef USE_INSTANCING
       instanceMatrix *
       #endif
-      modelViewMatrix * vec4(${position}, 1.0)
+      modelViewMatrix * ${castToVec4(position)}
     `,
     { varying: true }
   )
