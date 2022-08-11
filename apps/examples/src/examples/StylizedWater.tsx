01b8c7ee44e682eaa57f21a184fd1ad0a69b1b05
Hendrik Mans <hendrik@mans.de>
2022-08-11 06:44:54 +0200

    uniforms

--- apps/examples/src/examples/StylizedWater.tsx
+++ apps/examples/src/examples/StylizedWater.tsx
@@ -2,6 +2,7 @@ import { Float } from "@react-three/drei"
 import { MeshProps } from "@react-three/fiber"
 import { useControls } from "leva"
 import {
+  Abs,
   Add,
   CustomShaderMaterialMaster,
   Div,
@@ -19,7 +20,7 @@ import {
   VertexNormal,
   VertexPosition
 } from "shader-composer"
-import { useShader } from "shader-composer-r3f"
+import { useShader, useUniformUnit } from "shader-composer-r3f"
 import { PSRDNoise3D, SceneDepthTexture } from "shader-composer-toybox"
 import { Color, MeshStandardMaterial } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
@@ -41,20 +42,27 @@ export default function StylizedWater() {
 }
 
 const Water = (props: MeshProps) => {
-  const controls = useControls("Water", {})
+  const controls = useControls("Water", {
+    calmness: { value: 0.5, min: 0, max: 1 },
+    shallow: "#00877d",
+    deep: "#06192e",
+    foam: "#eef"
+  })
+
+  const calmness = useUniformUnit("float", controls.calmness)
+
+  const colors = {
+    shallow: useUniformUnit("vec3", new Color(controls.shallow)),
+    deep: useUniformUnit("vec3", new Color(controls.deep)),
+    foam: useUniformUnit("vec3", new Color(controls.foam))
+  }
 
   const shader = useShader(() => {
     const time = Time()
     const sceneDepthTexture = SceneDepthTexture({ layer: Layers.TransparentFX })
     const sceneDepth = PerspectiveDepth(ScreenUV, sceneDepthTexture)
 
-    const colors = {
-      shallow: Vec3(new Color("#058")),
-      deep: Vec3(new Color("#003366")),
-      foam: Vec3(new Color("#eef"))
-    }
-
-    const noise = Add(
+    const waveNoise = Add(
       PSRDNoise3D(Add(VertexPosition, time)),
       PSRDNoise3D(Sub(VertexPosition, time))
     )
@@ -65,12 +73,22 @@ const Water = (props: MeshProps) => {
       diffuseColor: pipe(
         colors.shallow,
         (v) => Mix(v, colors.deep, Smoothstep(3, 8, depth)),
-        (v) => Mix(colors.foam, v, Smoothstep(0.5, 0.51, depth))
+        (v) => Mix(colors.foam, v, Smoothstep(0.5, 1, depth))
       ),
 
       alpha: 0.9,
 
-      normal: Add(VertexNormal, Mul(noise, 0.2))
+      normal: pipe(VertexNormal, (v) =>
+        Add(
+          v,
+          pipe(
+            calmness,
+            (v) => OneMinus(v),
+            (v) => Mul(waveNoise, v),
+            (v) => Mul(v, 0.2)
+          )
+        )
+      )
     })
   }, [])
 
