8bcb7d66c050c2208545d14b362832cd892b6e4c
Hendrik Mans <hendrik@mans.de>
2022-08-11 15:09:16 +0200

    Cleanup

--- apps/examples/src/examples/StylizedWater.tsx
+++ apps/examples/src/examples/StylizedWater.tsx
@@ -18,7 +18,6 @@ import {
   Step,
   Sub,
   Time,
-  vec3,
   VertexNormal,
   VertexPosition
 } from "shader-composer"
@@ -44,8 +43,6 @@ export default function StylizedWater() {
 }
 
 const Water = (props: MeshProps) => {
-  const scene = useRenderPass({ layer: Layers.TransparentFX })
-
   const controls = useControls("Water", {
     calmness: { value: 0.5, min: 0, max: 1 },
     shallow: "#00877d",
@@ -61,26 +58,46 @@ const Water = (props: MeshProps) => {
     foam: useUniformUnit("vec3", new Color(controls.foam))
   }
 
+  /* Let's run a pre-pass that will render the scene, minus our
+  water, to render & depth textures that we will source later. */
+  const scene = useRenderPass({ layer: Layers.TransparentFX })
+
   const shader = useShader(() => {
+    /* Get a time uniform, always useful for time-based effects! */
     const time = Time()
 
+    /* Calculate some overlapping noise. We're going to use this
+    to change the geometry original normals and scene color UVs to
+    create the water surface effect. */
     const waveNoise = Add(
       PSRDNoise3D(Add(VertexPosition, time)),
       PSRDNoise3D(Sub(VertexPosition, time))
     )
 
+    /* Modify the screen UV based on the wave noise. */
     const refractedUV = pipe(
       waveNoise,
       (v) => Mul(v, 0.01),
       (v) => Add(ScreenUV, v)
     )
 
-    const sceneDepth = PerspectiveDepth(refractedUV, scene.depthTexture)
-    const depth = Sub(VertexPosition.view.z, sceneDepth)
+    /* Calculate the depth by comparing the current fragment's
+    depth in view space to the depth of the scene. */
+    const depth = pipe(
+      scene.depthTexture,
+      (v) => PerspectiveDepth(refractedUV, v),
+      (v) => Sub(VertexPosition.view.z, v)
+    )
 
+    /* Grab the original color from the pre-pass' render texture. */
+    const originalColor = SceneColor(refractedUV, scene.texture).color
+
+    /* Let's decide on some factors for specific "parts" of the water
+    surface by smooth-stepping the depth. */
     const deepFactor = Smoothstep(3, 8, depth)
     const foamFactor = Smoothstep(2, 0, depth)
 
+    /* Now let's create a foam texture based on noise. */
     const foam = pipe(
       VertexPosition,
       (v) => Add(v, Mul(time, 0.2)),
@@ -90,8 +107,8 @@ const Water = (props: MeshProps) => {
       (v) => Clamp(0, 0.8, v)
     )
 
-    const whiteFoam = Mul(foam, OneMinus(Step(0.3, depth)))
-
+    /* Calculate the extent to which we will be "distorting" the water
+    by changing its normals later */
     const surfaceDistortion = pipe(
       calmness,
       (v) => OneMinus(v),
@@ -113,8 +130,6 @@ const Water = (props: MeshProps) => {
       return Add(xyz, Mul(normal, Mul(scaledNoise, amount)))
     }
 
-    const originalColor = SceneColor(refractedUV, scene.texture).color
-
     return CustomShaderMaterialMaster({
       position: pipe(VertexPosition, (v) =>
         Waves(v, VertexNormal, time, OneMinus(calmness))
@@ -124,8 +139,7 @@ const Water = (props: MeshProps) => {
         colors.shallow,
         (v) => Mix(v, originalColor, 0.5),
         (v) => Mix(v, colors.deep, deepFactor),
-        (v) => Mix(v, colors.foam, foam),
-        (v) => Mix(v, Mul(colors.foam, 2), whiteFoam)
+        (v) => Mix(v, colors.foam, foam)
       ),
 
       roughness: foamFactor,
