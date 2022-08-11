5b1c593cceec1251e86757217dcb195bbd7b133f
Hendrik Mans <hendrik@mans.de>
2022-08-11 06:19:12 +0200

    Cool

--- apps/examples/src/examples/StylizedWater.tsx
+++ apps/examples/src/examples/StylizedWater.tsx
@@ -1,17 +1,35 @@
 import { Float } from "@react-three/drei"
 import { MeshProps } from "@react-three/fiber"
 import { useControls } from "leva"
-import { CustomShaderMaterialMaster, Time } from "shader-composer"
+import {
+  Add,
+  CustomShaderMaterialMaster,
+  Div,
+  Mix,
+  OneMinus,
+  PerspectiveDepth,
+  pipe,
+  Saturate,
+  ScreenUV,
+  Smoothstep,
+  Sub,
+  Time,
+  Vec3,
+  VertexPosition
+} from "shader-composer"
 import { useShader } from "shader-composer-r3f"
-import { MeshStandardMaterial } from "three"
+import { SceneDepthTexture } from "shader-composer-toybox"
+import { Color, MeshStandardMaterial } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
+import { Layers } from "../r3f-venue/Layers"
 
 export default function StylizedWater() {
   return (
     <group position-y={-2}>
-      <Water />
+      <Water layers={Layers.TransparentFX} />
       <Rock position={[-10, 0, -10]} scale={5} />
       <Rock position={[-10, -10, -10]} scale={10} />
+      <Rock position={[-5, -10, -5]} scale={6} />
       <Rock position={[-3, -2, -8]} scale={4} rotation-y={2} />
       <Float>
         <Rock position={[3, -1, -3]} scale={2} rotation-y={1} />
@@ -21,16 +39,29 @@ export default function StylizedWater() {
 }
 
 const Water = (props: MeshProps) => {
-  /* Expose some controls via Leva. */
   const controls = useControls("Water", {})
 
-  /* Create our shader. */
   const shader = useShader(() => {
-    /* Create a time unit. Always useful! */
     const time = Time()
+    const sceneDepthTexture = SceneDepthTexture({ layer: Layers.TransparentFX })
+    const sceneDepth = PerspectiveDepth(ScreenUV, sceneDepthTexture)
+
+    const colors = {
+      shallow: Vec3(new Color("#058")),
+      deep: Vec3(new Color("#003366")),
+      foam: Vec3(new Color("#eef"))
+    }
+
+    const depth = Sub(VertexPosition.view.z, sceneDepth)
 
     return CustomShaderMaterialMaster({
-      alpha: 0.5
+      diffuseColor: pipe(
+        colors.shallow,
+        (v) => Mix(v, colors.deep, Smoothstep(3, 8, depth)),
+        (v) => Mix(colors.foam, v, Smoothstep(0.5, 0.51, depth))
+      ),
+
+      alpha: 0.9
     })
   }, [])
 
