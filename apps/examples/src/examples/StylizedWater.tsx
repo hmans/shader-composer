08b5a8ea09f0ee05c5206a5cec9ccb4d166efc05
Hendrik Mans <hendrik@mans.de>
2022-08-11 05:53:48 +0200

    Add some rocks

--- apps/examples/src/examples/StylizedWater.tsx
+++ apps/examples/src/examples/StylizedWater.tsx
@@ -1,3 +1,5 @@
+import { Float } from "@react-three/drei"
+import { MeshProps } from "@react-three/fiber"
 import { useControls } from "leva"
 import { CustomShaderMaterialMaster, Time } from "shader-composer"
 import { useShader } from "shader-composer-r3f"
@@ -5,6 +7,20 @@ import { MeshStandardMaterial } from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 
 export default function StylizedWater() {
+  return (
+    <group position-y={-2}>
+      <Water />
+      <Rock position={[-10, 0, -10]} scale={5} />
+      <Rock position={[-10, -10, -10]} scale={10} />
+      <Rock position={[-3, -2, -8]} scale={4} rotation-y={2} />
+      <Float>
+        <Rock position={[3, -1, -3]} scale={2} rotation-y={1} />
+      </Float>
+    </group>
+  )
+}
+
+const Water = (props: MeshProps) => {
   /* Expose some controls via Leva. */
   const controls = useControls("Water", {})
 
@@ -13,13 +29,22 @@ export default function StylizedWater() {
     /* Create a time unit. Always useful! */
     const time = Time()
 
-    return CustomShaderMaterialMaster({})
+    return CustomShaderMaterialMaster({
+      alpha: 0.5
+    })
   }, [])
 
   return (
-    <mesh>
-      <icosahedronGeometry args={[1, 5]} />
-      <CustomShaderMaterial baseMaterial={MeshStandardMaterial} {...shader} />
+    <mesh {...props} rotation-x={-Math.PI / 2}>
+      <planeGeometry args={[32, 32, 50, 50]} />
+      <CustomShaderMaterial baseMaterial={MeshStandardMaterial} {...shader} transparent />
     </mesh>
   )
 }
+
+const Rock = (props: MeshProps) => (
+  <mesh {...props}>
+    <icosahedronGeometry args={[1, 0]} />
+    <meshStandardMaterial color="#555" />
+  </mesh>
+)
