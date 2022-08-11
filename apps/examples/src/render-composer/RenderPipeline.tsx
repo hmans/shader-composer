90f7242a10b2083a94a24395b9584205db6f284b
Hendrik Mans <hendrik@mans.de>
2022-08-11 19:50:07 +0200

    eh

--- apps/examples/src/render-composer/RenderPipeline.tsx
+++ apps/examples/src/render-composer/RenderPipeline.tsx
@@ -1,15 +1,27 @@
 import { useFrame, useThree } from "@react-three/fiber"
-import { EffectComposer } from "postprocessing"
+import { CopyPass, EffectComposer, NormalPass, TextureEffect } from "postprocessing"
 import { useLayoutEffect, useMemo } from "react"
+import { HalfFloatType } from "three"
 import { LayerRenderPass } from "./LayerRenderPass"
 
 export const RenderPipeline = () => {
   const { gl, scene, camera, size } = useThree()
 
-  const composer = useMemo(() => new EffectComposer(gl), [])
+  const composer = useMemo(
+    () => new EffectComposer(gl, { frameBufferType: HalfFloatType }),
+    []
+  )
 
   useLayoutEffect(() => {
-    composer.addPass(new LayerRenderPass(scene, camera, undefined, 0xffffff))
+    /* Render all layers except transparent FX. */
+    const preRenderPass = new LayerRenderPass(scene, camera, undefined, 1)
+    preRenderPass.clear = false
+    composer.addPass(preRenderPass)
+
+    /* Now render only transparent FX. */
+    const transparentFXPass = new LayerRenderPass(scene, camera, undefined, 2)
+    transparentFXPass.clear = false
+    composer.addPass(transparentFXPass)
 
     return () => composer.removeAllPasses()
   }, [composer, scene, camera])
