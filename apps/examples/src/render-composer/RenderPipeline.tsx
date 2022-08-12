59f7ce123528f0e75937373a4e8698c949786b06
Hendrik Mans <hendrik@mans.de>
2022-08-12 13:27:53 +0200

    Normal render pass... for now

--- apps/examples/src/render-composer/RenderPipeline.tsx
+++ apps/examples/src/render-composer/RenderPipeline.tsx
@@ -1,5 +1,5 @@
 import { useFrame, useThree } from "@react-three/fiber"
-import { EffectComposer } from "postprocessing"
+import { EffectComposer, RenderPass } from "postprocessing"
 import { useLayoutEffect, useMemo } from "react"
 import { HalfFloatType } from "three"
 import { LayerRenderPass } from "./LayerRenderPass"
@@ -15,14 +15,10 @@ export const RenderPipeline = () => {
   useLayoutEffect(() => {
     /* Render all layers except transparent FX. */
     const preRenderPass = new LayerRenderPass(scene, camera, undefined, 1)
-    preRenderPass.clear = false
     composer.addPass(preRenderPass)
 
-    /* Now render only transparent FX. */
-    const transparentFXPass = new LayerRenderPass(scene, camera, undefined, 2)
-    transparentFXPass.needsDepthTexture = true
-    transparentFXPass.clear = false
-    transparentFXPass.renderToScreen = true
+    /* Now render the full scene. */
+    const transparentFXPass = new RenderPass(scene, camera)
     composer.addPass(transparentFXPass)
 
     return () => composer.removeAllPasses()
