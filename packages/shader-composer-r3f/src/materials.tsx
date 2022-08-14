b93c2ace3e15706f132eb79794cc57189aa78330
Hendrik Mans <hendrik@mans.de>
2022-08-14 14:02:19 +0200

    Tweak prop type name

--- packages/shader-composer-r3f/src/materials.tsx
+++ packages/shader-composer-r3f/src/materials.tsx
@@ -3,11 +3,11 @@ import CustomShaderMaterial, { iCSMProps } from "three-custom-shader-material"
 import CustomShaderMaterialImpl from "three-custom-shader-material/vanilla"
 import { MeshDepthMaterial, RGBADepthPacking } from "three"
 
-export type CustomShaderMaterialProps = Omit<iCSMProps, "ref" | "baseMaterial">
+export type CustomDepthMaterialProps = Omit<iCSMProps, "ref" | "baseMaterial">
 
 export const CustomDepthMaterial = forwardRef<
   CustomShaderMaterialImpl,
-  CustomShaderMaterialProps
+  CustomDepthMaterialProps
 >((props, ref) => {
   const [material] = useState(
     () => new MeshDepthMaterial({ depthPacking: RGBADepthPacking })
